036efb41387ec749734afa130d737fe87478bf00
13020180055 <61979075+13020180055@users.noreply.github.com>
2020-05-05 05:46:29 -0700

    Add files via upload

---
+++ tugas 8 pbo/varStatic.java
@@ -0,0 +1,9 @@
+public class varStatic {
+    public static void main(String[] args){
+        String firstName = "BELLA ";
+        String lastName = "ARIVA";
+        String FULLname = firstName + lastName;
+        
+        System.out.println(FULLname);
+    }
+}
